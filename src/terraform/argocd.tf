ea11750e20c53fef00479c186e3487f9a4808ea6
Tristan Rivoallan <tristan@rivoallan.net>
2022-05-02 02:21:23 +0200

    tf: argocd

---
+++ src/terraform/argocd.tf
@@ -0,0 +1,53 @@
+resource "argocd_project" "pastis-hosting" {
+  metadata {
+    name      = "pastis-hosting"
+    namespace = "ph-argocd"
+  }
+
+  spec {
+    description  = "Pastis Hosting"
+    source_repos = ["*"]
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "*"
+    }
+    cluster_resource_blacklist {
+      group = "*"
+      kind  = "*"
+    }
+    cluster_resource_whitelist {
+      group = "rbac.authorization.k8s.io"
+      kind  = "ClusterRoleBinding"
+    }
+    cluster_resource_whitelist {
+      group = "rbac.authorization.k8s.io"
+      kind  = "ClusterRole"
+    }
+    namespace_resource_blacklist {
+      group = "networking.k8s.io"
+      kind  = "Ingress"
+    }
+    namespace_resource_whitelist {
+      group = "*"
+      kind  = "*"
+    }
+  }
+}
+
+resource "kubernetes_namespace" "ph-argocd" {
+  metadata {
+    annotations = {
+      name = "ph-argocd"
+    }
+
+    name = "ph-argocd"
+  }
+}
+
+# resource "helm_release" "argocd" {
+#   name       = "argocd"
+#   chart      = "./src/applications/enabled/argocd"
+#   namespace  = kubernetes_namespace.ph-argocd.metadata[0].annotations.name
+#   timeout    = 1800
+# }
