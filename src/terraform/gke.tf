b0a115adc670beaf1e340556ef7392a40e74ca76
Tristan Rivoallan <tristan@rivoallan.net>
2022-05-02 03:09:36 +0200

    tf: disable gke autopilot

--- src/terraform/gke.tf
+++ src/terraform/gke.tf
@@ -42,7 +42,7 @@ resource "google_container_cluster" "primary" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  enable_autopilot         = true
+  enable_autopilot         = false
   initial_node_count       = 1
 
   network    = google_compute_network.vpc.name
