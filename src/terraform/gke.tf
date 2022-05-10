8e2828a444130b0d70cadc6f5ea98fbff0df03f5
Tristan Rivoallan <tristan@rivoallan.net>
2022-05-10 04:59:25 +0200

    tf: project

--- src/terraform/gke.tf
+++ src/terraform/gke.tf
@@ -28,33 +28,22 @@ data "google_billing_account" "tristan" {
   open         = true
 }
 
-resource "google_project" "ph" {
-  name                = "Pastis Hosting"
-  project_id          = var.project_id
-  auto_create_network = false
-  billing_account     = data.google_billing_account.tristan.id
-
-  lifecycle {
-    ignore_changes = all
-  }
-}
-
 resource "google_project_service" "container" {
-  project                    = google_project.ph.project_id
+  project                    = var.project_id
   service                    = "container.googleapis.com"
   disable_dependent_services = true
 }
 
 resource "google_project_service" "compute" {
-  project                    = google_project.ph.project_id
+  project                    = var.project_id
   service                    = "compute.googleapis.com"
   disable_dependent_services = true
 }
 
 # GKE cluster
 resource "google_container_cluster" "primary" {
-  project  = google_project.ph.project_id
-  name     = "${google_project.ph.project_id}-gke"
+  project  = var.project_id
+  name     = "${var.project_id}-gke"
   location = var.gke_cluster_location
   
   # We can't create a cluster with no node pool defined, but we want to only use
@@ -73,7 +62,7 @@ resource "google_container_node_pool" "primary_nodes" {
   location   = var.gke_cluster_location
   cluster    = google_container_cluster.primary.name
   node_count = var.gke_num_nodes
-  project    = google_project.ph.project_id
+  project    = var.project_id
   autoscaling {
     min_node_count = 1
     max_node_count = 2
@@ -90,12 +79,12 @@ resource "google_container_node_pool" "primary_nodes" {
     ]
 
     labels = {
-      env = google_project.ph.project_id
+      env = var.project_id
     }
 
     # preemptible  = true
     machine_type = var.gke_machine_type
-    tags         = ["gke-node", "${google_project.ph.project_id}-gke"]
+    tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
     }
